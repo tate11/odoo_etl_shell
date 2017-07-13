818d46e8c1f9d004f84a23c7176addcc660de157
Mike Mugge <github@muggeltd.com>
2017-07-13 15:19:39 -0600

    WIP

---
+++ manifest.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo ETL Shell",
+
+    'application': True,
+
+    'summary': """
+        A rudimentary framework that serves as a basis for creating complex Odoo ETL modules""",
+
+    'description': """
+        A rudimentary framework that serves as a basis for creating complex Odoo ETL modules
+    """,
+
+    'author': "community",
+    'website': "https://github.com/idazco/odoo-etl-shell",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Technical Settings',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    # always loaded
+    'data': [],
+    'qweb': [],
+    # only loaded in demonstration mode
+    #'demo': [
+    #    'demo/demo.xml',
+    #],
+}
