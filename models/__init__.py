63c89c45e7caddf9a0cffffe9630eb099aca6112
Diogo Duarte <diogocarvalhoduarte@gmail.com>
2017-10-27 00:04:19 +0100

    Generalize the hoa list to key_list

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from . import data_mapping
 from . import res_config
 from . import cron_instance
 from . import cron_job
+from . import key_list
---
+++ models/key_list.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields
+
+
+class KeyList(models.Model):
+    _name = 'odoo_etl_shell.key_list'
+    _description = 'Key List'
+
+    list_name = fields.Char('List Name', required=True, default='white_list', invisible=True)
+    name = fields.Char('Value Name', required=True)
+    sync_hoa = fields.Boolean('Import', required=True, default=False)
+    module_name = fields.Char('Module', required=True)
